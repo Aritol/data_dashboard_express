4375ee2074f2785aa143a6f0bf30f82d759a93bf
Max Andreyanov <92523078+Aritol@users.noreply.github.com>
2024-06-01 14:55:34 +0300

    update backend

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -4,7 +4,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const { get } = require("lodash");
+const { get, isEmpty } = require("lodash");
 const cloudinary = require("../utils/cloudinary");
 
 const sendJSONResponse = (res, status, content) => {
@@ -28,8 +28,8 @@ module.exports.getList = function (req, res) {
 
 async function signup(req, res) {
     try {
-        const currentDate = new Date().getTime(); // Текущая дата и время в миллисекундах
-        const randomNumber = Math.floor(Math.random() * 1000000) + 10000; // Генерируем случайное число от 10000 до 999999
+        const currentDate = new Date().getTime(); // Текущя дата і час в мілісекундах
+        const randomNumber = Math.floor(Math.random() * 1000000) + 10000; //Генеруєм случайне число від 10000 до 999999
         const userId = currentDate.toString() + randomNumber.toString();
         const sliceNumber = Math.floor(Math.random() * 3) + 4;
         const user = new UsersModel({
@@ -55,13 +55,12 @@ async function signup(req, res) {
             token
         });
     } catch (err) {
+        console.log("err --->", err);
         return res.status(500).json({ error: "Signup error" });
     }
 }
 
 async function login(req, res) {
-    console.log("req.headers");
-    console.log(req.headers);
     try {
         if (!req.body.email || !req.body.email) {
             return res.status(401).json({ error: "fill required fiellds" });
@@ -94,6 +93,7 @@ async function login(req, res) {
             }
         });
     } catch (err) {
+        console.log("err --->", err);
         return res.status(500).json({ error: "Login error" });
     }
 }
@@ -107,13 +107,19 @@ const getUserData = async (req, res) => {
             });
         }
 
-        const users = await UsersModel.find({ userId }).select().exec();
-        const files = express.static(
-            path.join(__dirname, `images/avatars/${userId}`)
-        );
-        console.log("file");
-        console.log(files);
-        sendJSONResponse(res, 200, { success: true, data: users });
+        const user =
+            get(await UsersModel.find({ userId }).select().exec(), "[0]", {}) ||
+            {};
+        let userData = {};
+        if (user && !isEmpty(user)) {
+            userData = {
+                firstName: user.firstName || "",
+                lastName: user.lastName || "",
+                email: user.email || "",
+                photo: user.photo || ""
+            };
+        }
+        sendJSONResponse(res, 200, { success: true, userData });
     } catch (err) {
         console.error("Fetch failed:", err);
         sendJSONResponse(res, 500, {
@@ -125,14 +131,9 @@ const getUserData = async (req, res) => {
 
 const saveImage = async (req, res) => {
     const userId = get(req.user, "user_id", 0) || 0;
-
-    console.log("userId");
-    console.log(userId);
     if (userId) {
         const storage = multer.diskStorage({
             filename: (req, file, cb) => {
-                console.log("file.originalname");
-                console.log(file.originalname);
                 cb(null, file.originalname);
             }
         });
@@ -170,63 +171,6 @@ const saveImage = async (req, res) => {
     } else {
         return res.status(400).send({ error: "Користувач не авторизований" });
     }
-    // if (userId) {
-    //     // const storageDir = path.join(
-    //     //     __dirname,
-    //     //     `../../storage/images/avatars/${userId}`
-    //     // );
-    //     // const user = await UsersModel.findOne({ userId: userId });
-    //     // if (!user) {
-    //     //     console.error(" користувача не знайдено");
-    //     //     return res
-    //     //         .status(500)
-    //     //         .send({ error: "Помилка, користувача не знайдено" });
-    //     // }
-    //     // let fileName = "";
-    //     const storage = multer.diskStorage({
-    //         // destination: (req, file, cb) => {
-    //         //     cb(null, storageDir);
-    //         // },
-    //         filename: (req, file, cb) => {
-    //             // fileName = Date.now() + path.extname(file.originalname);
-    //             // console.log("fileName funcs");
-    //             // console.log(fileName);
-    //             // cb(null, Date.now() + path.extname(file.originalname));
-    //             console.log("file.originalname");
-    //             console.log(file.originalname);
-    //             cb(null, file.originalname);
-    //         }
-    //     });
-
-    //     const upload = multer({ storage: storage });
-    //     // // console.log(await storage.getFilename());
-
-    //     // if (!fs.existsSync(storageDir)) {
-    //     //     fs.mkdirSync(storageDir, { recursive: true });
-    //     // }
-    //     upload.single("image")(req, res, async (err) => {
-    //         if (err) {
-    //             console.error(err);
-    //             return res
-    //                 .status(500)
-    //                 .send({ error: "Помилка при завантаженні файлу" });
-    //         }
-    //         cloudinary.uploader.upload(req.file.path, function (error, result) {
-    //             if (error) {
-    //                 console.error(error);
-    //                 return res.status(500).send({
-    //                     error: "Помилка при завантаженні файлу на сервер"
-    //                 });
-    //             }
-    //         });
-    //         console.log("result");
-    //         console.log(result);
-    //         // user.photo = fileName;
-    //         // user.save();
-    //         return res.status(200).send({ uploadSuccess: true });
-    //     });
-    // }
-    // return res.status(500).send({ error: "Помилка" });
 };
 
 const deleteImage = async (req, res) => {
@@ -236,7 +180,7 @@ const deleteImage = async (req, res) => {
             const user = await UsersModel.findOne({ userId: userId });
             delete user.photo;
             await user.save();
-            return res.status(200).send({ success: "Помилка" });
+            return res.status(200).send({ success: true });
         } catch (error) {
             console.error("Виникла помилка:", error);
             return res.status(500).json({
